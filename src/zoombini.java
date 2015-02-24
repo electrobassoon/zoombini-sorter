4987a6e075830da3a275b0f852867f9ec4e60595
jazzbassoon <Bleeding.Flame@gmail.com>
2015-02-24 18:37:18 -0500

    zoombini class

--- src/zoombini.java
+++ src/zoombini.java
@@ -1,4 +1,43 @@
 
 public class zoombini {
 
+	private String hair;
+	private String eye;
+	private String nose;
+	private String feet;
+	
+	public zoombini(){	
+	}
+	
+	public zoombini(String hairType, String eyeType, String noseType, String feetType){
+		hair = hairType;
+		eye = eyeType;
+		nose = noseType;
+		feet = feetType;
+		
+	}
+	public String getHair() {
+		return hair;
+	}
+	public void setHair(String hairType) {
+		this.hair = hairType;
+	}
+	public String getEye() {
+		return eye;
+	}
+	public void setEye(String eyeType){
+		this.eye = eyeType;
+	}
+	public String getNose() {
+		return nose;
+	}
+	public void setNose(String noseType){
+		this.nose = noseType;
+	}
+	public String getFeet(){
+		return feet;
+	}
+	public void setFeet(String feetType){
+		this.feet = feetType;
+	}
 }
